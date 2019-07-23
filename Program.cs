0f1489399697652896b5d607819f792f3f6e210a
gugu <gustavocorrea2032@gmail.com>
2019-07-23 12:35:48 -0300

    CRCA

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FunçãoCRCA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] n = {50 , 6.78 , 10 , 7.7 , 86 , 7.77};
+            Console.WriteLine(soma(n));
+        }
+        static Double soma(double[] valores){
+            double rs = 0;
+            for(int i = 0; i < valores.Length; i++){
+                rs+=valores[i];
+            }
+            return rs;
+
+        }
+    }
+}
